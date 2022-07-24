00c007eec4923b056b259add5b964b0e7140b2dd
brianjia2020 <brianjia1123@gmail.com>
2022-07-24 10:35:14 -0400

    updated

---
+++ compile.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import shutil
+
+
+def run():
+    cwd = "/Users/chunyangjia/Desktop/Projects/C_learn/source_files"
+    outputFolder = "/Users/chunyangjia/Desktop/Projects/C_learn/output"
+    toCompile = findAllItems(cwd)
+    if os.path.isdir(outputFolder):
+        shutil.rmtree(outputFolder)
+        os.mkdir(outputFolder)
+    else:
+        os.mkdir(outputFolder)
+
+    for item in toCompile:
+        target = item.replace("source_files", "output")
+        target = target[:-2]
+        path, file = os.path.split(target)
+        print(path, file)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        subprocess.run(["clang", item, "-o", target])
+
+
+def findAllItems(cwd):
+    files = os.listdir(cwd)
+    # print(files)
+    toCompile = []
+    for item in files:
+        newItem = os.path.join(cwd, item)
+        # print(item.split("."))
+        # print(newItem)
+        if os.path.isfile(newItem) and item.split(".")[-1] == "c":
+            print(item)
+            toCompile.append(newItem)
+        if os.path.isdir(newItem):
+            toCompile.extend(findAllItems(newItem))
+    return toCompile
+
+
+if __name__ == "__main__":
+    run()
