74d801c8884654ce9fbe147d8e816407de06f977
Brian Jia <brianjia@fb.com>
2022-08-01 20:57:05 -0400

    updated

--- source_files/forceTypeConversion.c
+++ source_files/forceTypeConversion.c
@@ -4,5 +4,17 @@ int main()
 {
     double d1 = 1.234;
     int num = (int)d1;
+
+    // force type conversion
+    int num2 = (int) 3.5 * 10 + 6 * 1.5; // equals to 39
+    int num3 = (int) (3.5 * 10 + 6 * 1.5);
+    printf("This num2 is: %d\n", num2);
+    printf("This num3 is: %d\n", num3);
+
+    char c = 'a';
+    int i = 5;
+    float num4 = .345F;
+    double d = c + i + num4;
+    printf("This num3 is: %.6f\n", d);
     return 0;
 }
